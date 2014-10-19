432036ffe99d7507dbdb8fa84cc8ac82f40d723b
shu0 <ttt.sss80@gmail.com>
2014-10-20 02:49:42 +0900

    Update write.php

--- write.php
+++ write.php
@@ -1,6 +1,16 @@
 <?php
 include("common.php");
+
 if (isset($_POST["submit"])) {
+
+if (!isset($_POST["title"])) {
+$errors["title"] = "件名が入力されていません";
+} elseif (mb_strlen($_POST["title"]) == 0 ) {
+$errors["title"] = "件名が入力されていません";
+} elseif (mb_strlen($_POST["title"]) > 20 ) { 
+$errors["title"] = "件名は２０文字以内";
+}
+
 if (!isset($_POST["name"])) {
 $errors["name"] = "名前が入力されていません";
 } elseif (mb_strlen($_POST["name"]) == 0 ) {
@@ -8,6 +18,7 @@ $errors["name"] = "名前が入力されていません";
 } elseif (mb_strlen($_POST["name"]) > 20 ) { 
 $errors["name"] = "名前は２０文字以内";
 }
+
 if (!isset($_POST["comment"])) {
 $errors["comment"] = "コメントない";
 } elseif (mb_strlen($_POST["comment"]) == 0 ) {
@@ -18,15 +29,18 @@ $errors["comment"] = "コメント４００文字以内";
 } else {
 $errors["submit"] = "フォーム正しく送信されてない";
 }
+
 if (count($errors) == 0) {
 $result = bbs_write($_POST);
 if (!$result) {
 $errors["result"] = "書き込み失敗";
 }
 }
+
 if (count($errors) == 0) {
 header("Location: index.php");
 }
+
 ?>
 <!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
