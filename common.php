f2f51d2cc2633fe1e3c4c7d51d9c52d8e0ee4a25
shu0 <ttt.sss80@gmail.com>
2014-10-20 02:39:18 +0900

    Update common.php

--- common.php
+++ common.php
@@ -1,63 +1,52 @@
 <?php
-include("common.php");
-//
-if (isset($_POST["submit"])) {
-//
-if (!isset($_POST["name"])) {
-$errors["name"] = "名前が入力されていません";
-} elseif (mb_strlen($_POST["name"]) == 0 ) {
-$errors["name"] = "名前が入力されていません";
-} elseif (mb_strlen($_POST["name"]) > 20 ) { 
-$errors["name"] = "名前は２０文字以内";
-}
-//
-if (!isset($_POST["title"])) {
-$errors["title"] = "件名が入力されていません";
-} elseif (mb_strlen($_POST["title"]) == 0 ) {
-$errors["title"] = "件名が入力されていません";
-} elseif (mb_strlen($_POST["title"]) > 20 ) { 
-$errors["title"] = "件名は２０文字以内";
-}
-//
-if (!isset($_POST["comment"])) {
-$errors["comment"] = "コメントない";
-} elseif (mb_strlen($_POST["comment"]) == 0 ) {
-$errors["comment"] = "コメントない";
-} elseif (mb_strlen($_POST["comment"]) > 400) {
-$errors["comment"] = "コメント４００文字以内";
-}
-} else {
-$errors["submit"] = "フォーム正しく送信されてない";
-}
-//
-if (count($errors) == 0) {
-$result = bbs_write($_POST);
-if (!$result) {
-$errors["result"] = "書き込み失敗";
-}
-}
-//
-if (count($errors) == 0) {
-header("Location: index.php");
-}
-//
-?>
-<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>ERROR</title>
-<style>
-ul.error {color:red;}
-</style>
-</head>
-<body>
-<h1>ERROR</h1>
+ 
+mb_internal_encoding("UTF-8");
+mb_language("ja");
+setlocale(LC_ALL,"ja_JP.UTF-8");
 
-<ul class="error"><?php
-foreach($errors as $error) {
-?><li><?php print $error; ?></li><?php
-} ?></ul>
-<a href="index.php">BACK</a>
-</body>
-</html>
+
+$bbs_file = "bbs.csv";
+
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+  }
+  
+  function bbs_write($data)
+  {
+  global $bbs_file;
+  
+  $handle = fopen($bbs_file, "a");
+  
+  $data["comment"] = str_replace(array("\r\n", "\n", "\r"), PHP_EOL,$data["comment"]);
+  
+  $csv[] = $data["title"];
+  $csv[] = $data["name"];
+  $csv[] = $data["comment"];
+  $csv[] = time();
+  
+    $result = fputcsv($handle,$csv);
+  
+     fclose($handle);
+  
+     return $result;
+}
+
+function bbs_read()
+{
+  global $bbs_file;
+  
+  $handle = fopen($bbs_file,"r");
+  
+  while ($csv = fgetcsv($handle)) {
+      $record["title"] = $csv[0];
+      $record["name"] = $csv[1];
+      $record["comment"] = $csv[2];
+      $record["time"] = $csv[3];
+      $records[] =  $record;
+}
+
+  fclose($handle);
+
+  return $records;
+ }
+ ?>
